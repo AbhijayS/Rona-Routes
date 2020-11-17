ecf3570bc64adc1c7c89f7f74a52c6044c50146f
AbhijayS <abhijay.saini@gmail.com>
2020-11-17 16:23:31 -0600

    date time estimates

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ var carButton = document.getElementById("car-button");
 var walkButton = document.getElementById("walk-button");
 var transport = carButton;
 var searchButton = document.getElementById("search-button");
-var toLngLat, fromLngLat;
+var toLngLat, fromLngLat, toCounty, allRoutes;
+var routeRatingEl = document.getElementById("route-rating");
+var routeDurationEl = document.getElementById("route-duration");
+var routeArrivalEl = document.getElementById("route-arrival");
 
 walkButton.onclick = () => {
   walkButton.querySelector("path").setAttribute("fill", "black");
@@ -26,8 +29,15 @@ var fromSearch = places({
   apiKey: 'c06e6514463c64853edb7dcfd882d774',
   container: document.querySelector('#from-input')
 })
-toSearch.on('change', e => toLngLat = [e.suggestion.latlng.lng, e.suggestion.latlng.lat]);
-fromSearch.on('change', e => fromLngLat = [e.suggestion.latlng.lng, e.suggestion.latlng.lat]);
+toSearch.on('change', e => {
+  toLngLat = [e.suggestion.latlng.lng, e.suggestion.latlng.lat];
+  toCounty = e.suggestion.county;
+  map.flyTo({center: [toLngLat[0], toLngLat[1]], zoom: 9});
+});
+fromSearch.on('change', e => {
+  fromLngLat = [e.suggestion.latlng.lng, e.suggestion.latlng.lat]
+  map.flyTo({center: [fromLngLat[0], fromLngLat[1]], zoom: 9});
+});
 
 /* MapBox */
 mapboxgl.accessToken = 'pk.eyJ1IjoiYXMyMDAxIiwiYSI6ImNraGZkbHVwNzBiZTQydnBjbXFuNXA0ZTAifQ.E0nuXPtQYbmI_6r7KGqKjg';
@@ -40,33 +50,26 @@ var map = new mapboxgl.Map({
 
 map.on('load', () => {
   searchButton.onclick = () => {
-  
     function doneStartPoint() {
       if (map.getLayer('start') && map.isSourceLoaded('start')) {
         map.off('sourcedata', doneStartPoint);
-  
         // add end point
         map.on('sourcedata', doneEndPoint);
         addEndPoint();
       }
     }
-  
     function doneEndPoint() {
       if (map.getLayer('end') && map.isSourceLoaded('end')) {
         map.off('sourcedata', doneEndPoint);
-        
+        // add directions   
         if (!map.getLayer('route')) {
           addRoute();
         }
-  
         addRoute();
       }
     }
-  
     map.on('sourcedata', doneStartPoint);
-  
     addStartPoint();
-  
   }
 });
 
@@ -144,6 +147,7 @@ function addRoute() {
   req.open('GET', url, true);
   req.onload = function () {
     var json = JSON.parse(req.response);
+    allRoutes = json;
     var data = json.routes[0];
     var route = data.geometry.coordinates;
     var geojson = {
@@ -185,4 +189,25 @@ function addRoute() {
     }
   };
   req.send();
+
+  function doneRoute() {
+    if (map.getSource('route') && map.isSourceLoaded('route')) {
+      updateEstimates();
+      map.off('sourcedata', doneRoute);
+    }
+  }
+  map.on('sourcedata', doneRoute);
+
+  map.fitBounds([toLngLat, fromLngLat], {padding: 20});
+
+}
+
+function updateEstimates() {
+  // request rating
+  console.log(allRoutes);
+  var duration = allRoutes.routes[0].duration;
+  routeDurationEl.innerText = (Math.floor(duration/3600)!=0?`${Math.floor(allRoutes.routes[0].duration/3600)} h ` : '') + `${Math.floor(((allRoutes.routes[0].duration/3600) - Math.floor(allRoutes.routes[0].duration/3600))*60)} m`;
+  var arrival = new Date(Date.now() + duration*1000);
+  var now = new Date();
+  routeArrivalEl.innerText = (arrival.getDate()!=now.getDate())?arrival.toLocaleString():arrival.toLocaleTimeString();
 }
