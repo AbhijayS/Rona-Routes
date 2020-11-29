b57815202ee201f7a3dec0ea2a59b280d34b1e4a
AbhijayS <abhijay.saini@gmail.com>
2020-11-28 19:13:47 -0600

    heatmap needs revision

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ var toLngLat, fromLngLat, toCounty, allRoutes;
 var routeRatingEl = document.getElementById("route-rating");
 var routeDurationEl = document.getElementById("route-duration");
 var routeArrivalEl = document.getElementById("route-arrival");
+var heatMapLayers = [];
 
 walkButton.onclick = () => {
   walkButton.querySelector("path").setAttribute("fill", "black");
@@ -98,51 +99,68 @@ map.on('load', () => {
   //   }
   // })
 
-  var poly = {
-    type: 'geojson',
-    data: 'http://localhost:8080/US_Counties.json'
-    // data: {
-    //   type: 'Feature',
-    //   geometry: {
-    //     type: 'Polygon',
-    //     coordinates: [
-    //       [
-    //         [-67.13734351262877, 45.137451890638886],
-    //         [-66.96466, 44.8097],
-    //         [-68.03252, 44.3252],
-    //         [-69.06, 43.98],
-    //         [-70.11617, 43.68405],
-    //         [-70.64573401557249, 43.090083319667144],
-    //         [-70.75102474636725, 43.08003225358635],
-    //         [-70.79761105007827, 43.21973948828747],
-    //         [-70.98176001655037, 43.36789581966826],
-    //         [-70.94416541205806, 43.46633942318431],
-    //         [-71.08482, 45.3052400000002],
-    //         [-70.6600225491012, 45.46022288673396],
-    //         [-70.30495378282376, 45.914794623389355],
-    //         [-70.00014034695016, 46.69317088478567],
-    //         [-69.23708614772835, 47.44777598732787],
-    //         [-68.90478084987546, 47.184794623394396],
-    //         [-68.23430497910454, 47.35462921812177],
-    //         [-67.79035274928509, 47.066248887716995],
-    //         [-67.79141211614706, 45.702585354182816],
-    //         [-67.13734351262877, 45.137451890638886]
-    //       ]
-    //     ]
-    //   }
-    // }
-  };
+  var countiesJSONURL = 'http://localhost:8080/US_Counties.json';
+  fetch(countiesJSONURL)
+    .then(res => res.json())
+    .then(async (countiesData) => {
+      await Promise.all(
+        countiesData.features.map(mapboxSourceObject => {
+          // making rating request
+          var ratingColor;
+          var ratingsURL = "http://localhost:8080/countyheatmapdata.json";
+          return fetch(ratingsURL)
+            .then(res => res.json())
+            .then(allRatings => {
+              var county = allRatings.find(r => r['county'] == mapboxSourceObject.properties.NAME + ' County');
+              ratingColor = applyColor(county['rating']);
+              // create then append layer
+              var mapboxLayer = {
+                id: mapboxSourceObject.properties.NAME,
+                type: 'fill',
+                source: mapboxSourceObject,
+                layout: {},
+                paint: {
+                  'fill-color': ratingColor,
+                  'fill-opacity': 0.8
+                }
+              };
+              heatMapLayers.push(mapboxLayer);
+            })
+            .catch(() => {
+              console.error('Ratings response error.');
+              ratingColor = applyColor(1);
+              map.addSource(mapboxSourceObject.properties.NAME, mapboxSourceObject);
+              var mapboxLayer = {
+                id: mapboxSourceObject.properties.NAME,
+                type: 'fill',
+                source: mapboxSourceObject.properties.NAME,
+                layout: {},
+                paint: {
+                  'fill-color': ratingColor,
+                  'fill-opacity': 0.8
+                }
+              };
+              heatMapLayers.push(mapboxLayer);
+            })
+        })
+      );
+      // setup heatmap button
+      console.log(heatMapLayers);
 
-  map.addSource('poly', poly);
-  map.addLayer({
-    id: 'poly-layer',
-    type: 'fill',
-    source: 'poly',
-    'paint': {
-      'fill-color': '#088',
-      'fill-opacity': 0.8
-    }
-  })
+      // for testing
+      map.addLayer(heatMapLayers[0]);
+    });
+
+  // map.addSource('poly', poly);
+  // map.addLayer({
+  //   id: 'poly-layer',
+  //   type: 'fill',
+  //   source: 'poly',
+  //   'paint': {
+  //     'fill-color': '#088',
+  //     'fill-opacity': 0.8
+  //   }
+  // });
 
   searchButton.onclick = () => {
     function doneStartPoint() {
@@ -349,7 +367,7 @@ function updateEstimates(routeID) {
 function applyColor(value) {
   value = Math.floor(value);
   switch (value) {
-    case 1: return "#ACB334"
+    case 1: return "#69B34C"
     case 2: return "#FAB733"
     case 3: return "#FF8E15"
     case 4: return "#FF4E11"
